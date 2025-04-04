6678fffa833bd02645e5fe5d2a72f1d4b0e56065
peppercOdes <yil53066@gmail.com>
2025-04-04 17:25:08 -0400

    Initial Commit

--- WeekNumber/Week10/HW/HW.js
+++ WeekNumber/Week10/HW/HW.js
@@ -2,6 +2,9 @@ var isOn = true; // Global variable
 var name = "Yi Li";
 var age = 18;
 var favNumber = 3.1;
+const numberCalculate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var calculateResult
+
 
 function helloWorld() {
     console.log("My first JavaScript assignment");
@@ -21,13 +24,21 @@ function variablePractice() {
     console.log("My name is " + name + ". I am " + age + " years old and my favorite number is " + favNumber + ".");
 }
 
-function calculate(a, b) {
+function calculate(operation) {
+    var a = [Math.floor(Math.random() * numberCalculate.length)];
+    var b = [Math.floor(Math.random() * numberCalculate.length)];
+    if (operation === "add") {
+        calculateResult = a + b;
+    } else if (operation === "subtract") {
+        calculateResult = a - b;
+    } else if (operation === "multiply") {
+        calculateResult = a * b;
+    } else if (operation === "divide") {
+        calculateResult = a / b;
+    } 
 
-    let c = a + b;
-    let d = a - b;
-    let e = a * b;
-    let f = a / b;
-
+    console.log(a + " " + b)
+    console.log("Result: " + Math.floor(calculateResult));
 }
 
 
